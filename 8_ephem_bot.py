dbbe6b3cfc516378ac5588f307f3eac54fa49cfb
Anton Semenov <superjuk@gmail.com>
2021-02-24 21:03:58 +0300

    8_ephem_bot.py complete

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -12,7 +12,11 @@
   бота отвечать, в каком созвездии сегодня находится планета.
 
 """
+import ephem
 import logging
+import settings as stg
+import datetime as dt
+import planets_list as pll
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
@@ -39,14 +43,51 @@ def greet_user(update, context):
 def talk_to_me(update, context):
     user_text = update.message.text
     print(user_text)
-    update.message.reply_text(text)
+    update.message.reply_text(user_text)
+
+def show_constellation(update, context):
+    text = update.message.text.lower().split()
+    if len(text) != 2:
+        update.message.reply_text('Неверно задана планета. Попробуйте ещё раз.')
+        return
+    
+    currentDate = dt.date.today().strftime('%Y/%m/%d')
+    planet = ephem.Planet
+    if text[1] == 'mercury':
+        planet = ephem.Mercury(currentDate)
+    elif text[1] == 'venus':
+        planet = ephem.Venus(currentDate)
+    elif text[1] == 'mars':
+        planet = ephem.Mars(currentDate)
+    elif text[1] == 'jupiter':
+        planet = ephem.Jupiter(currentDate)
+    elif text[1] == 'saturn':
+        planet = ephem.Saturn(currentDate)
+    elif text[1] == 'uranus':
+        planet = ephem.Uranus(currentDate)
+    elif text[1] == 'neptune':
+        planet = ephem.Neptune(currentDate)
+    elif text[1] == 'pluto':
+        planet = ephem.Pluto(currentDate)
+    else:
+        response = f'Планета введена неверно. Повторите ввод.\n Список планет: {pll.planets}'
+        print(response)
+        update.message.reply_text(response)
+        return
+
+    constellation = ephem.constellation(planet)
+    print(constellation)
+    response = f'{pll.planets_rus.get(planet.name, planet.name)} сегодня находится в созвездии {constellation[1]}.'
+    update.message.reply_text(response)
+
 
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
+    mybot = Updater(stg.token, request_kwargs=PROXY, use_context=True)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", show_constellation))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     mybot.start_polling()
---
+++ planets_list.py
@@ -0,0 +1,2 @@
+planets = ['Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Pluto']
+planets_rus = {'Mercury':'Меркурий', 'Venus':'Венера', 'Mars':'Марс', 'Jupiter':'Юпитер', 'Saturn':'Сатурн', 'Uranus':'Уран', 'Neptune':'Нептун', 'Pluto':'Плутон'}
